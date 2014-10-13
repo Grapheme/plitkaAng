6a8a1adbcb75b6eb9509836a432b9cdebed03165
Artem Tumin <arttumin@gmail.com>
2014-10-13 18:51:23 +0400

    main page ready

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the plitkaApp
  */
 angular.module('plitkaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+		//Fotorama init
+		jQuery('.fotorama').fotorama({
+			width: '100%',
+			height: '400',
+			nav: false,
+			arrows: 'always'
+		});
+	});
