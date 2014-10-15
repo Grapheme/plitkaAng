085028c55c8be85a4c017622fae58d38712e2031
Artem Tumin <arttumin@gmail.com>
2014-10-15 13:17:42 +0400

    Separate project / separate article

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,6 +65,7 @@ module.exports = function (grunt) {
         },
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/views/partials/*.html',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
